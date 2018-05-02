e34bbdb8dfb6e81a0601923d2402886e091a40c5
kylinhuang <kylinhuang1029755440@gmail.com>
2018-05-02 09:49:20 +0800

    # 添加注释

--- flutterdemo/lib/main.dart
+++ flutterdemo/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter ',
+      title: 'Flutter ', // 多任务 title
       theme: new ThemeData(
         // This is the theme of your application.
         //
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Pa'),
+      home: new MyHomePage(title: 'Flutter Demo Home Pa'), // home 主屏幕
     );
   }
 }
